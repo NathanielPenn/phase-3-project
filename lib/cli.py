f6fd74894a9c764a551a3dcc991283d83d847582
Jim Brennan <jim.j.brennan@gmail.com>
2023-03-08 17:40:17 -0500

    we got our CLI and Helper functions working

--- lib/cli.py
+++ lib/cli.py
@@ -1,67 +1,77 @@
 #!/usr/bin/env python3
 
 # run cli.py
-#   select Fish
-#       select a location
-#           prints bait/lure you need and what the price and all that 
 #   Select Location
 #       Choose a Season
 #           select a Fish
 #               prints row from the fishes table 
 
 
+# print message "here are the locations "
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db.models import Store
-from helpers import (create_store_table, create_grocery_item_table,
-                     fill_cart, show_cart, remove_from_cart, collect_payment)
+from db.models import Fish, Location, Bait
+from helpers import (create_location_table, create_fish_table)
+#fill_cart, show_cart, remove_from_cart, collect_payment)
 
-engine = create_engine('sqlite:///db/grocery_stores.db')
+engine = create_engine('sqlite:///db/stardew.db')
 session = sessionmaker(bind=engine)()
 
 if __name__ == '__main__':
-    # Intro: welcome to the CLI, pick a store
+    # Intro: welcome to the CLI, pick a Fishing Location
     print('''
-  ________                                                                       
- /  _____/______  ____   ____  ___________ ___.__.                               
-/   \  __\_  __ \/  _ \_/ ___\/ __ \_  __ <   |  |                               
-\    \_\  \  | \(  <_> )  \__\  ___/|  | \/\___  |                               
- \______  /__|   \____/ \___  >___  >__|   / ____|                               
-        \/                  \/    \/       \/                                    
-_________ .__                   __                 __    _________ .____    .___ 
-\_   ___ \|  |__   ____   ____ |  | ______  __ ___/  |_  \_   ___ \|    |   |   |
-/    \  \/|  |  \_/ __ \_/ ___\|  |/ /  _ \|  |  \   __\ /    \  \/|    |   |   |
-\     \___|   Y  \  ___/\  \___|    <  <_> )  |  /|  |   \     \___|    |___|   |
- \______  /___|  /\___  >\___  >__|_ \____/|____/ |__|    \______  /_______ \___|
-        \/     \/     \/     \/     \/                           \/        \/    
+    ___     _                         _                            __   __            _       _              _  _  
+  / __|   | |_    __ _      _ _   __| |    ___   __ __ __   o O O \ \ / /  __ _     | |     | |     ___    | || | 
+  \__ \   |  _|  / _` |    | '_| / _` |   / -_)  \ V  V /  o       \ V /  / _` |    | |     | |    / -_)    \_, | 
+  |___/   _\__|  \__,_|   _|_|_  \__,_|   \___|   \_/\_/  TS__[O]  _\_/_  \__,_|   _|_|_   _|_|_   \___|   _|__/  
+_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| {======|_| """"|_|"""""|_|"""""|_|"""""|_|"""""|_| """"| 
+"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'./o--000'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
 
 ''')
-    print('Hello! Welcome to the grocery checkout CLI.')
-    print('Here is a list of available stores:')
-    stores = session.query(Store)
-    create_store_table(stores)
+    print('Hello! Welcome to Stardew Valley. \n\n')
+    print('Here is a list of fishing spots:')
+    locations = session.query(Location)
+    create_location_table(locations)
 
-    # Get a choice of store, retrieve an object from the DB
-    store = None
-    while not store:
-        store_id = input('Please enter the ID of the store where you wish to shop: ')
-        store = session.query(Store).filter(Store.id == store_id).one_or_none()
+    # Get a choice of location, retrieve an object from the DB
+    location = None
+    #we need to increase scope of location_id
+    location_id = None
+    while not location:
+        location_id = input('Please enter the ID of the location you would like to go fishing: ')
+        location = session.query(Location).filter(Location.id == location_id).one_or_none()
+            
+    
+    # Display list of items at the store
+    # needs to show list of fish available at the location
+    print('\n\nHere are all the fish you can catch at this location:')
+    #this is where we need to put our filter
+    fishes = session.query(Fish).filter(Fish.location_id == location_id)
+    create_fish_table(fishes)
+
+
+    fish = None
+    while not fish:
+        fish_id = input('\n\nPlease enter the ID of the Fish you would like to catch:')
+        fish = session.query(Fish).filter(Fish.id == fish_id).one_or_none()
+        print(fish)
 
     # Display list of items at the store
-    print('Here is a list of our offerings:')
-    create_grocery_item_table(store)
+    # print('Here are all the fish you can catch at this location:')
+    # create_fish_table(fish)
 
-    # Start adding items to cart
-    shopping_cart, cart_total = fill_cart(session, store)
-    print('Here are the items in your cart:')
-    show_cart(shopping_cart)
+    # # Start adding items to cart
+    # shopping_cart, cart_total = fill_cart(session, store)
+    # print('Here are the items in your cart:')
+    # show_cart(shopping_cart)
 
-    # Remove unwanted items from cart
-    remove_from_cart(session, shopping_cart, cart_total)
+    # # Remove unwanted items from cart
+    # remove_from_cart(session, shopping_cart, cart_total)
 
     # Collect payment
-    print(f'Your total is ${cart_total:.2f}\n')
-    collect_payment(cart_total)
+    # print(f'Your total is ${cart_total:.2f}\n')
+    # collect_payment(cart_total)
 
     print('Thank you for using the grocery checkout CLI!\n')
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,105 +1,123 @@
-from db.models import GroceryItem, ShoppingCart
+from db.models import Fish, Location, Bait
+
+#   Select Location
+#       Choose a Season
+#           select a Fish
+#               prints row from the fishes table 
+
+#   "Welcome!"
+#       Give list of locations
+#           "Select Location by ID"
+#               we take input => Give list of fish from that location.id
+#                   "select a Fish by ID"
+#                       we take input =>
+#                           prints f"you selected ${fish.name}! If you catch one it'll be worth PRICE. You need to use {bait.name} to catch it! Good luck!"
+
 
 YES = ['y', 'ye', 'yes']
 NO = ['n', 'no']
 
-def create_store_table(stores):
+#this will print out the locations
+def create_location_table(locations):
     print('-' * 50)
-    print(f'|ID  |NAME{" " * 39}|')
+    print(f'|ID  |LOCATION NAME{" " * 30}|')
     print('-' * 50)
-    for store in stores:
-        id_spaces = 4 - len(str(store.id))
-        name_spaces = 43 - len(store.name)
-        print(f'|{store.id}{" " * id_spaces}|{store.name}{" " * name_spaces}|')
-    print('-' * 50)
-
-def create_grocery_item_table(store):
-    print('-' * 50)
-    print(f'|ID  |ITEM NAME{" " * 24}|PRICE{" " * 4}|')
-    print('-' * 50)
-    for grocery_item in sorted(store.grocery_items, key=lambda g: g.id):
-        id_spaces = 4 - len(str(grocery_item.id))
-        name_spaces = 33 - len(grocery_item.name)
-        price_spaces = 8 - len(f'{grocery_item.price:.2f}')
-        output_string = f'|{grocery_item.id}{" " * id_spaces}|' + \
-            f'{grocery_item.name}{" " * name_spaces}|' + \
-            f'${grocery_item.price:.2f}{" " * price_spaces}|'
-        print(output_string)
+    for location in locations:
+        id_spaces = 4 - len(str(location.id))
+        name_spaces = 43 - len(location.name)
+        print(f'|{location.id}{" " * id_spaces}|{location.name}{" " * name_spaces}|')
     print('-' * 50)
 
-def fill_cart(session, store):
-    shopping_cart = ShoppingCart(store=store)
-    grocery_item_id = input('Please enter the ID of your first item: ')
-    cart_total = 0
-    while grocery_item_id:
-        grocery_item = session.query(GroceryItem).filter(
-            GroceryItem.id==grocery_item_id).first()
-        if grocery_item in store.grocery_items:
-            shopping_cart.grocery_items.append(grocery_item)
-            cart_total += grocery_item.price
-            print(f'Cart total is now ${cart_total:.2f}\n')
-        else:
-            grocery_item_id = input('Please enter a valid grocery item ID: ')
-            continue
 
-        yes_no = None
-        while yes_no not in YES + NO:
-            yes_no = input('Would you like to add another item to your cart? (Y/n) ')
-            if yes_no.lower() in YES:
-                grocery_item_id = input('Please enter the ID of your next item: ')
-            elif yes_no.lower() in NO:
-                grocery_item_id = None
+# we need this table to print out fish available at the location selected by input?
+# filter fish table by location.id
+#    print(fish instances)
 
-    return shopping_cart, cart_total
-
-def show_cart(shopping_cart):
+def create_fish_table(fishes):
     print('-' * 50)
-    print(f'|ID  |NAME{" " * 29}|PRICE{" " * 4}|')
+    print(f'|ID  |FISH NAME{" " * 34}|')
     print('-' * 50)
-    for grocery_item in sorted(shopping_cart.grocery_items, key=lambda g: g.id):
-        id_spaces = 4 - len(str(grocery_item.id))
-        name_spaces = 33 - len(grocery_item.name)
-        price_spaces = 8 - len(f'{grocery_item.price:.2f}')
-        output_string = f'|{grocery_item.id}{" " * id_spaces}|' + \
-            f'{grocery_item.name}{" " * name_spaces}|' + \
-            f'${grocery_item.price:.2f}{" " * price_spaces}|'
-        print(output_string)
-    cart_total = sum([g.price for g in shopping_cart.grocery_items])
-    total_spaces = 8 - len(str(cart_total))
-    print(f'|{" " * 5}TOTAL{" " * 28}|${cart_total:.2f}{" " * total_spaces}|')
+    for fish in fishes:
+        # we need to grab input...not use 2
+        id_spaces = 4 - len(str(fish.id))
+        name_spaces = 43 - len(fish.name)
+        print(f'|{fish.id}{" " * id_spaces}|{fish.name}{" " * name_spaces}|')
     print('-' * 50)
 
-def remove_from_cart(session, shopping_cart, cart_total):
-    yes_no = input('Would you like to remove any items from your cart? (Y/n) ')
-    while yes_no in YES:
-        grocery_item_id = input('Please enter the ID of the item you would like to remove: ')
-        grocery_item = session.query(GroceryItem).filter(
-            GroceryItem.id==grocery_item_id).first()
-        if grocery_item in shopping_cart.grocery_items:
-            shopping_cart.grocery_items.remove(grocery_item)
-            cart_total -= grocery_item.price
-        else:
-            print('Item not found.')
-        print('Here are the items in your cart:')
-        show_cart(shopping_cart)
 
-        yes_no = input('Would you like to remove another item from your cart? (Y/n) ')
+# def fill_cart(session, store):
+#     shopping_cart = ShoppingCart(store=store)
+#     grocery_item_id = input('Please enter the ID of your first item: ')
+#     cart_total = 0
+#     while grocery_item_id:
+#         grocery_item = session.query(GroceryItem).filter(
+#             GroceryItem.id==grocery_item_id).first()
+#         if grocery_item in store.grocery_items:
+#             shopping_cart.grocery_items.append(grocery_item)
+#             cart_total += grocery_item.price
+#             print(f'Cart total is now ${cart_total:.2f}\n')
+#         else:
+#             grocery_item_id = input('Please enter a valid grocery item ID: ')
+#             continue
 
-def collect_payment(cart_total):
-    paid = False
-    while not paid:
-        payment_method = input(f'Will you be paying with cash or card? ')
-        if payment_method.lower() == 'card':
-            print('Processing...\n')
-            paid = True
-        elif payment_method.lower() == 'cash':
-            payment = input('How much will you be paying with today? ' )
-            try:
-                payment = float(payment)
-                change = payment - cart_total
-                print(f'Your change is ${change:.2f}\n')
-                paid = True
-            except:
-                print('Please enter a valid number.')
-        else:
-            print('Please select a valid payment method.')
+#         yes_no = None
+#         while yes_no not in YES + NO:
+#             yes_no = input('Would you like to add another item to your cart? (Y/n) ')
+#             if yes_no.lower() in YES:
+#                 grocery_item_id = input('Please enter the ID of your next item: ')
+#             elif yes_no.lower() in NO:
+#                 grocery_item_id = None
+
+#     return shopping_cart, cart_total
+
+# def show_cart(shopping_cart):
+#     print('-' * 50)
+#     print(f'|ID  |NAME{" " * 29}|PRICE{" " * 4}|')
+#     print('-' * 50)
+#     for grocery_item in sorted(shopping_cart.grocery_items, key=lambda g: g.id):
+#         id_spaces = 4 - len(str(grocery_item.id))
+#         name_spaces = 33 - len(grocery_item.name)
+#         price_spaces = 8 - len(f'{grocery_item.price:.2f}')
+#         output_string = f'|{grocery_item.id}{" " * id_spaces}|' + \
+#             f'{grocery_item.name}{" " * name_spaces}|' + \
+#             f'${grocery_item.price:.2f}{" " * price_spaces}|'
+#         print(output_string)
+#     cart_total = sum([g.price for g in shopping_cart.grocery_items])
+#     total_spaces = 8 - len(str(cart_total))
+#     print(f'|{" " * 5}TOTAL{" " * 28}|${cart_total:.2f}{" " * total_spaces}|')
+#     print('-' * 50)
+
+# def remove_from_cart(session, shopping_cart, cart_total):
+#     yes_no = input('Would you like to remove any items from your cart? (Y/n) ')
+#     while yes_no in YES:
+#         grocery_item_id = input('Please enter the ID of the item you would like to remove: ')
+#         grocery_item = session.query(GroceryItem).filter(
+#             GroceryItem.id==grocery_item_id).first()
+#         if grocery_item in shopping_cart.grocery_items:
+#             shopping_cart.grocery_items.remove(grocery_item)
+#             cart_total -= grocery_item.price
+#         else:
+#             print('Item not found.')
+#         print('Here are the items in your cart:')
+#         show_cart(shopping_cart)
+
+#         yes_no = input('Would you like to remove another item from your cart? (Y/n) ')
+
+# def collect_payment(cart_total):
+#     paid = False
+#     while not paid:
+#         payment_method = input(f'Will you be paying with cash or card? ')
+#         if payment_method.lower() == 'card':
+#             print('Processing...\n')
+#             paid = True
+#         elif payment_method.lower() == 'cash':
+#             payment = input('How much will you be paying with today? ' )
+#             try:
+#                 payment = float(payment)
+#                 change = payment - cart_total
+#                 print(f'Your change is ${change:.2f}\n')
+#                 paid = True
+#             except:
+#                 print('Please enter a valid number.')
+#         else:
+#             print('Please select a valid payment method.')
